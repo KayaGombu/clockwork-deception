bc776a5da5b744b91789e95d4ffd8135d3d67b8f
Quinton <quintontram10@gmail.com>
2025-03-16 13:26:44 -0400

    Changing the vision cone code a bit

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 
 
 @onready var sprite: Sprite2D = $Icon
+@onready var area: Area2D = $Area2D
+@onready var collision: CollisionPolygon2D = $Area2D/CollisionPolygon2D
 @onready var ray_cast: RayCast2D = $Icon/RayCast2D
 @onready var ray_cast2: RayCast2D = $Icon/RayCast2D2
 @onready var ray_cast3: RayCast2D = $Icon/RayCast2D3
@@ -26,23 +28,24 @@ var current_state = States.WANDER
 func _ready():
 	left_bounds = self.position+Vector2(-120,0)
 	right_bounds = self.position+Vector2(120,0)
-	
+	area.connect("body_entered",Callable(self, "_on_area_2d_body_entered"))
+	area.connect("body_exited",Callable(self, "_on_area_2d_body_exited"))
 func _physics_process(delta: float) -> void:
 	movement(delta)
 	change_direction()
 	look_for_player()
 	
 func look_for_player():
-	if ray_cast.is_colliding() or ray_cast2.is_colliding() or ray_cast3.is_colliding():
-		var collider = ray_cast.get_collider()
-		var collider2 = ray_cast2.get_collider()
-		var collider3 = ray_cast3.get_collider()
-		if collider == player or collider2 == player or collider3 == player:
-			chase_player()
-		elif current_state == States.CHASE:
-			stop_chase()
-	elif current_state == States.CHASE:
-		stop_chase()
+	for ray in [ray_cast,ray_cast2,ray_cast3]:
+		if ray.is_colliding():
+			var collider = ray_cast.get_collider()
+		
+			if collider == player:
+				chase_player()
+			elif current_state == States.CHASE:
+				stop_chase()
+	
+	stop_chase()
 		
 func chase_player():
 	timer.stop()
@@ -73,6 +76,7 @@ func change_direction() -> void:
 				ray_cast.target_position = Vector2(-200,0)
 				ray_cast2.target_position = Vector2(-200,20)
 				ray_cast3.target_position = Vector2(-200,-20)
+				flip_cone(true)
 		else:
 			
 			if self.position.x>=left_bounds.x:
@@ -82,6 +86,7 @@ func change_direction() -> void:
 				ray_cast.target_position = Vector2(200,0)
 				ray_cast2.target_position = Vector2(200,20)
 				ray_cast3.target_position = Vector2(200,-20)
+				flip_cone(false)
 	else:
 		direction = (player.position - self.position).normalized()
 		direction = sign(direction)
@@ -90,12 +95,19 @@ func change_direction() -> void:
 			ray_cast.target_position = Vector2(200,0)
 			ray_cast2.target_position = Vector2(200,30)
 			ray_cast3.target_position = Vector2(200,-30)
+			flip_cone(true)
 		else: 
 			sprite.flip_h = false
 			ray_cast.target_position = Vector2(-200,0)
 			ray_cast2.target_position = Vector2(-200,20)
 			ray_cast3.target_position = Vector2(-200,-20)
-			
+			flip_cone(false)
+func flip_cone(is_facing_right: bool):
+	var scale =1 if is_facing_right else -1
+	area.scale.x = scale
+	for i in range(collision.polygon.size()):
+		collision.polygon[i].x *=1
+	
 #const SPEED = 300.0
 
 #var vision_cone_angle = deg_to_rad(100.0)
@@ -130,5 +142,15 @@ func change_direction() -> void:
 
 func _on_timer_timeout() -> void:
 	current_state = States.WANDER
-	if current_state == States.CHASE:
-		current_state == States.WANDER
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		chase_player()
+		collision.modulate = Color(0, 1, 0)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		chase_player()
+		collision.modulate = Color(1, 1, 1)
