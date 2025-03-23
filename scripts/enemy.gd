3c38f5099a2271f3b457b167bd09b98ca020352d
Quinton <quintontram10@gmail.com>
2025-03-22 21:27:13 -0400

    Very basic pathfinding
    Basic pathfinding for the enemy ai

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,82 +1,104 @@
 extends CharacterBody2D
 @export var player: CharacterBody2D
 @export var walls: StaticBody2D
-@export var SPEED: int = 50
-@export var CHASE_SPEED: int = 100
-@export var ACCELERATION: int = 160
+@export var SPEED: int = 40
+@export var CHASE_SPEED: int = 60
+@export var ACCELERATION: int = 65
+@onready var nav_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 
 
-#@onready var space_state = get_world_2d().direct_space_state
 @onready var sprite: Sprite2D = $Icon
-@onready var area: Area2D = $Area2D
-@onready var vision: Polygon2D = $Area2D/Polygon2D
-@onready var collision: CollisionPolygon2D = $Area2D/CollisionPolygon2D
+@onready var area: Area2D = $aggro/Area2D
+@onready var collision: CollisionPolygon2D = $aggro/Area2D/CollisionPolygon2D
 @onready var ray_cast: RayCast2D = $Icon/RayCast2D
 @onready var ray_cast2: RayCast2D = $Icon/RayCast2D2
 @onready var ray_cast3: RayCast2D = $Icon/RayCast2D3
-@onready var timer: Timer = $Timer
+@onready var recalcTimer: Timer = $Navigation/recalcTimer
 
+
+var home_pos = Vector2.ZERO
 var direction: Vector2
 var right_bounds: Vector2
 var left_bounds: Vector2
-var FOV_length := 190
-var vision_angle := 35
+
 
 enum States{
 	WANDER,
 	CHASE
 }
-
+var chasing_player = false
 var current_state = States.WANDER
 var vision_points = []
 func _ready():
+	home_pos = self.global_position
 	left_bounds = self.position+Vector2(-120,0)
 	right_bounds = self.position+Vector2(120,0)
 	area.connect("body_entered",Callable(self, "_on_area_2d_body_entered"))
 	area.connect("body_exited",Callable(self, "_on_area_2d_body_exited"))
-	
-	
+	recalcTimer.connect("timeout", Callable(self, "recalc_path"))
+	for ray in sprite.get_children():
+		if ray is RayCast2D:
+			ray.collision_mask = player.collision_layer
 func _physics_process(delta: float) -> void:
+	if current_state == States.CHASE:
+		recalc_path()
 	movement(delta)
+	move_and_slide()
 	change_direction()
 	look_for_player()
-	update_vision()
+
 func look_for_player():
-	for ray in [ray_cast,ray_cast2,ray_cast3]:
-		ray.force_raycast_update()
-		if ray.is_colliding():
-			var collider = ray_cast.get_collider()
-			if collider == null:
-				continue
-			if collider == walls:
-				print("Wall detected, stopping chase!")
-				
-				stop_chase()
-				return
-			elif collider == player:
-				print("player detected, chasing !")
-				chase_player()
-				return
-	
-	stop_chase()
-		
+#This function uses raycasts to detect players and walls
+	var player_spotted = false
+	for ray in sprite.get_children():
+		if ray is RayCast2D:
+			ray.enabled = true
+			if ray.is_colliding() and ray.get_collider() == player:
+				player_spotted = true
+				break
+	if player_spotted: #once it spots the enemy, initate chase state
+		chase_player()
+	else:
+		pass
+
+
 func chase_player():
-	timer.stop()
+#this function starts the chase state
 	current_state = States.CHASE
+	chasing_player = true
+	recalc_path()#finds the best path to chase the player
 	
 func stop_chase():
-	#if timer.time_left<=0:
-		#timer.start()
-	if current_state == States.CHASE:
-		timer.start()
+#this function starts the timer and ends the chase state
+	current_state = States.WANDER
+	chasing_player = false
+	recalc_path()
+		
 func movement(delta: float) -> void:
+#this function handles the movement of the enemy
+	print("Current Direction: ", direction)
 	if current_state == States.WANDER:
 		velocity = velocity.move_toward(direction*SPEED, ACCELERATION*delta)
-	else:
-		velocity = velocity.move_toward(direction*CHASE_SPEED, ACCELERATION*delta)
-	move_and_slide()
+	elif current_state == States.CHASE:
+		if nav_agent.is_navigation_finished():
+			return
+		var next_path_pos = nav_agent.get_next_path_position()
+		var newDirection = (next_path_pos - global_position).normalized()
+		velocity = velocity.move_toward(newDirection*CHASE_SPEED, ACCELERATION*delta)
+		print("🟠 Moving towards:", next_path_pos, " Current Pos:", global_position, "Direction:", direction)
+		
 	
+	
+func recalc_path():
+#every 0.5 seconds, this determines the best possible path to get to the player
+	if current_state == States.CHASE and player:
+		nav_agent.target_position = player.global_position
+	else:
+		nav_agent.target_position = home_pos
+	
+	print("New target position:", nav_agent.target_position)
 func change_direction() -> void:
+	#this function changes the direction of the enemy
 	#when it's in a wander state
 	if current_state == States.WANDER:
 		#when we change this, it could be based on animations
@@ -102,8 +124,8 @@ func change_direction() -> void:
 				flip_cone(false)
 	else:
 		direction = (player.position - self.position).normalized()
-		direction = sign(direction)
-		if direction.x ==1:
+		#direction = sign(direction)
+		if direction.x >0:
 			sprite.flip_h = true
 			ray_cast.target_position = Vector2(200,0)
 			ray_cast2.target_position = Vector2(200,30)
@@ -115,86 +137,13 @@ func change_direction() -> void:
 			ray_cast2.target_position = Vector2(-200,20)
 			ray_cast3.target_position = Vector2(-200,-20)
 			flip_cone(true)
-	update_vision()
+	print("New Direction: ", direction)
 func flip_cone(is_facing_right: bool):
+#this function flips the vision cone
 	var scale =1 if is_facing_right else -1
 	area.scale.x = scale
-	vision.scale.x = scale 
-	
-func update_vision():
-	var current = $Icon.position
-	var pos = current
-	vision_points.clear()
-	vision_points.append(pos)
-	
-	var pointLeft = pos +Vector2(FOV_length,vision_angle).rotated(direction.angle())
-	var pointright = pos +Vector2(FOV_length,-vision_angle).rotated(direction.angle())
-	
-	vision_points.append(pointLeft)
-	vision_points.append(pointright)
-	#set_target_area(PackedVector2Array(vision_points))
-#func get_FOV(from: Vector2, radius: float, width: float, angle: float) ->PackedVector2Array:
-	#var points = PackedVector2Array()
-	#var startingAngle = angle - width/2
-	#var endAngle = angle+width/2
-	#
-	#while startingAngle< endAngle:
-		#var offset = Vector2(radius,0).rotated(startingAngle)
-		#var to = from +offset
-		#var query = PhysicsRayQueryParameters2D.create(from, to)
-		#query.collide_with_bodies = true
-		#query.collide_with_areas = false  # Change to `true` if you want it to detect areas too
-		#var result = space_state.intersect_ray(query)
-		#if result:
-			#points.append(result.position)
-		#else:
-			#points.append(to)
-		#startingAngle +=FOV_length
-	#return points
-#func draw_target():
-	#var directionAngle = direction.angle()
-	#var vision_points = get_FOV($Icon.global_position, 250, 60, directionAngle)
-	#set_target_area(PackedVector2Array(vision_points))
-#func set_target_area(points: PackedVector2Array):
-	#vision.polygon = points
-#func clear_target_area():
-	#set_target_area(PackedVector2Array())
-#const SPEED = 300.0
-
-#var vision_cone_angle = deg_to_rad(100.0)
-#var angle_between_rays = deg_to_rad(5.0)
-#var rayList = []
-#
-#signal player_spotted
-#func _ready() -> void:
-#
-	#gen_raycasts()
-#
-#func _physics_process(delta: float) -> void:
-	#$"Icon".set_modulate(Color(1,1,1,1))
-	#for ray in rayList:
-		#if ray.is_colliding() and ray.get_collider() is Player:
-			#$"Icon".set_modulate(Color(100,1,1,1))
-			#player_spotted.emit()
-		#
-#
-#func gen_raycasts() -> void:
-	#var raycount = 4
-	#for i in raycount:
-		#var ray = RayCast2D.new()
-		#var angle = angle_between_rays*(i-raycount/2.0)
-		#ray.target_position = Vector2.RIGHT.rotated(angle)*360
-		#rayList.append(ray)
-		#add_child(ray)
-		#ray.enabled = true
-		
-	
-
-
-func _on_timer_timeout() -> void:
-	current_state = States.WANDER
-	
-
+	#for i in range(collision.polygon.size()):
+		#collision.polygon[i].x *= 1
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == player:
@@ -205,3 +154,10 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body == player:
 		chase_player()
 		collision.modulate = Color(1, 1, 1)
+
+func _on_deaggro_range_area_exited(area: Area2D) -> void:
+	#once it gets out of range (Deaggro Range), it goes back to the original position
+	if area.owner ==player:
+		current_state = States.WANDER
+		stop_chase()
+		recalc_path()
