e8be1f89448d0ed29929fe9960730d0eedc7da9c
KayaGombu <kayagombu@gmail.com>
2025-03-24 23:16:25 -0400

    a

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,15 +1,13 @@
 extends CharacterBody2D
 @export var player: CharacterBody2D
 @export var walls: StaticBody2D
-@export var SPEED: int = 40
+@export var SPEED: int = 100
 @export var CHASE_SPEED: int = 60
 @export var ACCELERATION: int = 65
 @onready var nav_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 
 
 @onready var sprite: Sprite2D = $Icon
-@onready var area: Area2D = $aggro/Area2D
-@onready var collision: CollisionPolygon2D = $aggro/Area2D/CollisionPolygon2D
 @onready var ray_cast: RayCast2D = $Icon/RayCast2D
 @onready var ray_cast2: RayCast2D = $Icon/RayCast2D2
 @onready var ray_cast3: RayCast2D = $Icon/RayCast2D3
@@ -24,7 +22,8 @@ var entityDetection := {}
 
 enum States{
 	WANDER,
-	CHASE
+	CHASE,
+	RETURN
 }
 var chasing_player = false
 var current_state = States.WANDER
@@ -33,8 +32,6 @@ func _ready():
 	home_pos = self.global_position
 	left_bounds = self.position+Vector2(-120,0)
 	right_bounds = self.position+Vector2(120,0)
-	area.connect("body_entered",Callable(self, "_on_area_2d_body_entered"))
-	area.connect("body_exited",Callable(self, "_on_area_2d_body_exited"))
 	recalcTimer.connect("timeout", Callable(self, "recalc_path"))
 	$"aggro/Deaggro Range".connect("body_exited", Callable(self, "_on_deaggro_body_exited"))
 	for ray in sprite.get_children():
@@ -42,10 +39,10 @@ func _ready():
 			ray.collision_mask = player.collision_layer
 func _physics_process(delta: float) -> void:
 	if current_state == States.CHASE:
-		recalc_path()
+		print("chasing")
+		chase_player(delta)
 	movement(delta)
 	move_and_slide()
-	change_direction()
 	look_for_player()
 
 func look_for_player():
@@ -56,115 +53,72 @@ func look_for_player():
 			ray.enabled = true
 			if ray.is_colliding() and ray.get_collider() == player:
 				player_spotted = true
+				print("player spotted")
 				break
 	if player_spotted: #once it spots the enemy, initate chase state
-		chase_player()
+		current_state = States.CHASE
 	else:
 		pass
 
 
-func chase_player():
+func chase_player(delta: float) -> void:
 #this function starts the chase state
-	if not chasing_player:
-		current_state = States.CHASE
-		chasing_player = true
-		recalc_path()#finds the best path to chase the player
+	#finds the best path to chase the player
+	nav_agent.target_position = player.global_position
+	if nav_agent.is_navigation_finished():
+		return
+	var next_path_pos = nav_agent.get_next_path_position()
+	var newDirection = (next_path_pos - global_position).normalized()
+	velocity = velocity.move_toward(newDirection*CHASE_SPEED, ACCELERATION*delta)
+	print("*** Moving towards:", next_path_pos, " Current Pos:", global_position, "Direction:", direction)
 	
 func stop_chase():
 #this function starts the timer and ends the chase state
-	if chasing_player:
-		chasing_player =false
-		current_state = States.WANDER
-		nav_agent.target_position = home_pos
-		recalc_path()
+	current_state = States.RETURN
+	nav_agent.target_position = get_parent().global_position
 		
 func movement(delta: float) -> void:
 #this function handles the movement of the enemy
-	#print("Current Direction: ", direction)
 	if current_state == States.WANDER:
-		velocity = velocity.move_toward(direction*SPEED, ACCELERATION*delta)
-	elif current_state == States.CHASE:
+		position = Vector2(0,0)
+		#print("moving")
+		get_parent().progress += SPEED/100.0
+	if current_state == States.RETURN:
 		if nav_agent.is_navigation_finished():
+			rotation = deg_to_rad(90)
+			current_state = States.WANDER
 			return
 		var next_path_pos = nav_agent.get_next_path_position()
-		var newDirection = (next_path_pos - global_position).normalized()
-		velocity = velocity.move_toward(newDirection*CHASE_SPEED, ACCELERATION*delta)
-		#print("*** Moving towards:", next_path_pos, " Current Pos:", global_position, "Direction:", direction)
+		velocity = global_position.direction_to(next_path_pos) *SPEED 
+		move_and_slide()
+		print("*** Moving towards:", next_path_pos, " Current Pos:", global_position)
+	print(get_parent().position,"  ",position)
 		
-	
+
 	
 func recalc_path():
 #every 0.5 seconds, this determines the best possible path to get to the player
 	if current_state == States.CHASE and player:
-		nav_agent.target_position = player.global_position
-	else:
-		nav_agent.target_position = home_pos
+		pass
+		print("Recalc New target position:", nav_agent.target_position)
+		
 	
-	print("New target position:", nav_agent.target_position)
-func change_direction() -> void:
-	#this function changes the direction of the enemy
-	#when it's in a wander state
-	if current_state == States.WANDER:
-		#when we change this, it could be based on animations
-		#flips the raycast
-		if sprite.flip_h:
-			if self.position.x<=right_bounds.x:
-				direction = Vector2(1,0)
-			else:
-				sprite.flip_h = false
-				ray_cast.target_position = Vector2(-190,0)
-				ray_cast2.target_position = Vector2(-190,20)
-				ray_cast3.target_position = Vector2(-190,-20)
-				flip_cone(true)
-		else:
-			
-			if self.position.x>=left_bounds.x:
-				direction = Vector2(-1,0)
-			else:
-				sprite.flip_h = true
-				ray_cast.target_position = Vector2(190,0)
-				ray_cast2.target_position = Vector2(190,20)
-				ray_cast3.target_position = Vector2(190,-20)
-				flip_cone(false)
-	else:
-		direction = (player.position - self.position).normalized()
-		#direction = sign(direction)
-		if direction.x >0:
-			sprite.flip_h = true
-			ray_cast.target_position = Vector2(200,0)
-			ray_cast2.target_position = Vector2(200,30)
-			ray_cast3.target_position = Vector2(200,-30)
-			flip_cone(false)
-		else: 
-			sprite.flip_h = false
-			ray_cast.target_position = Vector2(-200,0)
-			ray_cast2.target_position = Vector2(-200,20)
-			ray_cast3.target_position = Vector2(-200,-20)
-			flip_cone(true)
-	#print("New Direction: ", direction)
-func flip_cone(is_facing_right: bool):
-#this function flips the vision cone
-	var scale =1 if is_facing_right else -1
-	area.scale.x = scale
-	#for i in range(collision.polygon.size()):
-		#collision.polygon[i].x *= 1
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	entityDetection[body] = true
-	if body == player:
-		chase_player()
-		collision.modulate = Color(0, 1, 0)
-
-#func _on_area_2d_body_exited(body: Node2D) -> void:
-	#if body == player:
-		#chase_player()
-		#collision.modulate = Color(1, 1, 1)
+func move_ray(dir) -> void:
+		if dir == "up":
+			for ray in sprite.get_children():
+				ray.target_position = Vector2.UP.rotated(ray.target_position.angle())				
+		elif dir == "down":
+			for ray in sprite.get_children():
+				ray.target_position = Vector2.DOWN.rotated(ray.target_position.angle())
+		elif dir == "left":
+			for ray in sprite.get_children():
+				ray.target_position = Vector2.LEFT.rotated(ray.target_position.angle())			
+		elif dir == "right":
+			for ray in sprite.get_children():
+				ray.target_position = Vector2.RIGHT.rotated(ray.target_position.angle())	
 
 
 func _on_deaggro_range_body_exited(body: Node2D) -> void:
 	print("Exited:", body.name)
 	if body ==player:
-		print("Player left deAggro area")
 		stop_chase()
-		current_state = States.WANDER
-		recalc_path()
--- scripts/sabotage.gd
+++ scripts/sabotage.gd
@@ -1,6 +1,5 @@
 extends StaticBody2D
 @export var player: CharacterBody2D
-
 func _ready():
 	$sabotage_popup.visible = false
 
@@ -11,3 +10,9 @@ func _on_area_2d_body_entered(body):
 
 func _on_area_2d_body_exited(body):
 	$sabotage_popup.visible = false
+	
+func _physics_process(delta):
+	$bomb_object.visible = false
+	if Global.button_pressed == true:
+		Global.bomb_visible = true
+		$bomb_object.visible = true	
