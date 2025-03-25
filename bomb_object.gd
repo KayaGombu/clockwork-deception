f686d733d9f3550cba6d009116c43452d2a797a6
LaylaSambo <layla.sambo06@gmail.com>
2025-03-24 22:41:50 -0400

    Bomb

--- bomb_object.gd
+++ bomb_object.gd
@@ -1,5 +1,9 @@
 extends StaticBody2D
 
 func ready():
+	pass
+
+func _process(delta):
 	$bomb_object.visible = true
-	$bomb_object.play("default")
+	$bomb_object.play("explode")
+	
--- sabotage_popup.gd
+++ sabotage_popup.gd
@@ -1,20 +1,12 @@
 extends StaticBody2D
 
-
 var bomb = 1
-func _ready():
-	$bomb_icon.play("bomb")
-	bomb = 1
-	#create the code for the sabotage popup being visible when within range
 
-func _process(delta):
-	if self.visible == true:
-		if bomb == 1:
-			$bomb_icon.play("bomb")
-		
+func _ready():
+	pass
 
 func _on_sabotage_button_pressed():
-	Global.button_pressed = true
 	Global.bomb_visible = true
-	print("Sabotage pressed") 
+	print("Sabotage pressed")
+
 	
