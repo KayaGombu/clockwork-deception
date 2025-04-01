dea912e77e7d82c2d510c71aacc759cdcde5faa2
LaylaSambo <layla.sambo06@gmail.com>
2025-04-01 17:58:34 -0400

    Revert "Reapply "bomb fixed""
    This reverts commit 8be9118e6e5d7967a63f3bd303467798d1af1174.

--- area_2d.gd
+++
@@ -1,5 +0,0 @@
-class_name Interactable extends Area2D
-
-@export var interact_label = ""
-@export var interact_type = ""
-@export var interact_value = ""
--- scripts/sabotage_popup.gd
+++ scripts/sabotage_popup.gd
@@ -4,12 +4,18 @@ extends StaticBody2D
 var bomb = 1
 var animation_played = false
 func _ready():
-	pass
+	$bomb_icon.play("bomb")
+	bomb = 1
 	#create the code for the sabotage popup being visible when within range
 
 func _physics_process(delta):
-	pass
-
+	
+	if self.visible and bomb ==1:
+		if bomb == 1:
+			$bomb_icon.play("bomb")
+			animation_played = true
+			bomb =-1
+		
 
 func _on_sabotage_button_pressed():
 	Global.button_pressed = true
