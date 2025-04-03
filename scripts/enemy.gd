23a6f5d508901a57fc99ac55c3364947d639a016
Quinton <quintontram10@gmail.com>
2025-04-03 17:47:08 -0400

    Changes to the movement, path, and enemy sprite
    Smoother movement and animations

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 @export var player: CharacterBody2D
 @export var walls: StaticBody2D
 @export var SPEED: int = 100
-@export var CHASE_SPEED: int = 60
+@export var CHASE_SPEED: int = 35
 @export var ACCELERATION: int = 65
 @onready var nav_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 
@@ -36,12 +36,18 @@ func _ready():
 		if ray is RayCast2D:
 			ray.collision_mask = player.collision_layer
 func _physics_process(delta: float) -> void:
+	if velocity.length() > 0.1:  # Avoid rotating when idle
+		var target_angle = velocity.angle()
+		self.rotation = lerp_angle(self.rotation, target_angle, 5 * delta)
 	if current_state == States.CHASE:
 		print("chasing")
 		chase_player(delta)
+	update_animation()
 	movement(delta)
 	move_and_slide()
 	look_for_player()
+	self.rotation = 0
+	$AnimatedSprite2D.rotation = 0
 
 func look_for_player():
 #This function uses raycasts to detect players and walls
@@ -51,14 +57,14 @@ func look_for_player():
 			#print("collide")
 			if ray.get_collider() == player:
 				player_spotted = true
+				$AnimatedSprite2D.rotation = 0
+				update_animation()
 				print("player spotted")
 				break
 	if player_spotted: #once it spots the enemy, initate chase state
 		current_state = States.CHASE
 	else:
 		pass
-
-
 func chase_player(delta: float) -> void:
 #this function starts the chase state
 	#finds the best path to chase the player
@@ -68,13 +74,16 @@ func chase_player(delta: float) -> void:
 	var next_path_pos = nav_agent.get_next_path_position()
 	var newDirection = (next_path_pos - global_position).normalized()
 	velocity = velocity.move_toward(newDirection*CHASE_SPEED, ACCELERATION*delta)
+	$AnimatedSprite2D.rotation = 0
+	move_and_slide()
+	
 	
 func stop_chase():
 #this function starts the timer and ends the chase state
 	current_state = States.RETURN
 	nav_agent.target_position = get_parent().global_position
 		
-func movement(sdelta: float) -> void:
+func movement(delta: float) -> void:
 #this function handles the movement of the enemy
 	if current_state == States.WANDER:
 		position = Vector2(0,0)
@@ -87,7 +96,31 @@ func movement(sdelta: float) -> void:
 			return
 		var next_path_pos = nav_agent.get_next_path_position()
 		velocity = global_position.direction_to(next_path_pos) * SPEED/5
-		move_and_slide()
+		var newDirection = global_position.direction_to(next_path_pos)
+		if move_and_slide():
+			velocity = velocity.slide(get_last_slide_collision().get_normal())
+func update_animation():#get rid of function if you want to remove the animatedsprite
+	var NewMovement = velocity
+	if current_state == States.WANDER:
+		var path_follow = get_parent()
+		var prev_pos = path_follow.global_position
+		path_follow.progress += SPEED / 550.0
+		var new_pos = path_follow.global_position
+		NewMovement = (new_pos - prev_pos).normalized() * SPEED
+		path_follow.rotation = 0 
+		self.rotation = 0
+	if abs(NewMovement.x) > abs(NewMovement.y):  # Moving more horizontally
+		if NewMovement.x > 0:
+			$AnimatedSprite2D.play("right")
+		elif NewMovement.x<0:
+			$AnimatedSprite2D.play("left")
+	else:  # Moving more vertically
+		if NewMovement.y < 0:
+			$AnimatedSprite2D.play("walk")  # Up
+		elif NewMovement.y>0:
+			$AnimatedSprite2D.play("down")  # Down
+	
+	$AnimatedSprite2D.rotation = 0
 
 func gen_raycasts():
 	var cone_angle = deg_to_rad(54.0)
