8376759cdd547841a08be4a906cf95baf6bae8c7
SandwichThief <lukian.petryk@gmail.com>
2025-04-06 12:40:36 -0400

    Simple Game Menu + little changes
    Added 2 new nodes/scenes
    game_menu
    controls
    
    Changed:
    @onready var interactLabel = $"Interaction Components/InteractLabel"
    
    to
    
    @onready var interactLabel = $InteractionComponents/InteractLabel

---
+++ game_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://controls.tscn")
