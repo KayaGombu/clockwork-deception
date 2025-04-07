de7e2f883d89aac303999a8dbbdcc92f8442a2b9
KayaGombu <kayagombu@gmail.com>
2025-04-07 04:33:35 -0400

    a

--- TimerUI.gd
+++ TimerUI.gd
@@ -1,11 +1,16 @@
 extends Node2D
-
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
+var time 
+var sabotage_count = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
-	pass
+	time = $Timer.time_left
+	$"UI/Panel/TimerLabel".text = str(floor(time*2))
+	$UI/Panel/SabotageLabel.text = "Sabotages:" + str(sabotage_count)
+
+
+	
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://gameover.tscn")
+		
